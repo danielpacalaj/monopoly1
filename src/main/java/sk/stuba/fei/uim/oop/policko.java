31ae8e27a6d518dee4cf02b739feb07c6f752b20
Daniel <xpacalaj@stuba.sk>
2021-03-14 14:34:57 +0100

    policko a budova 1 commit

---
+++ src/main/java/sk/stuba/fei/uim/oop/policko.java
@@ -0,0 +1,11 @@
+package sk.stuba.fei.uim.oop;
+
+public class policko {
+    public String meno;
+    public int poziciaNaPloche;
+
+    public policko(String meno,int poziciaNaPloche){
+        this.meno = meno;
+        this.poziciaNaPloche = poziciaNaPloche;
+    }
+}
