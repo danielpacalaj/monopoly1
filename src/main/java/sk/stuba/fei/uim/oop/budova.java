a982b5ce6dae72353d928bb90e9a0e28d03152cc
Daniel <xpacalaj@stuba.sk>
2021-03-14 17:37:05 +0100

    budova dan policko a hrac 4. commit

--- src/main/java/sk/stuba/fei/uim/oop/budova.java
+++ src/main/java/sk/stuba/fei/uim/oop/budova.java
@@ -5,14 +5,14 @@ public class budova  extends policko{
     public String meno;
     public int cena;
     public int najom;
-    public String majitel;
+    public hrac majitel;
 
-    public budova(String meno,int poziciaNaPloche,int cena,int najom,String majitel){
+    public budova(String meno,int poziciaNaPloche,int cena,int najom){
         super(poziciaNaPloche);
         this.meno = meno;
         this.cena = cena;
         this.najom = najom;
-        this.majitel = majitel;
+        this.majitel = null;
     }
 
 }
--- src/main/java/sk/stuba/fei/uim/oop/zaplatDan.java
+++ src/main/java/sk/stuba/fei/uim/oop/zaplatDan.java
@@ -2,11 +2,11 @@ package sk.stuba.fei.uim.oop;
 
 public class zaplatDan extends policko{
 
-    public String meno = "Zaplat dan";
+    public String meno ;
 
     public zaplatDan(int poziciaNaPloche){
         super(poziciaNaPloche);
-        this.meno = meno;
+        this.meno = "Zaplat dan!";
     }
 
     public void zaplat(){
